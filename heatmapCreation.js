8c5299f1a5b7a6c9106305776fd1df461cdbf345
atticus cull <atticus@atticuss-MBP.lan>
2020-05-23 20:07:42 -0700

    Saving changes

--- heatmapCreation.js
+++ heatmapCreation.js
@@ -5,15 +5,17 @@ datapoints = [];
 var addDatapoint = function(latitude, longitude, heaviness = Weight) {
     datapoints.push({location: new google.maps.LatLng(latitude, longitude), weight: heaviness});
 }
-//datapoints.push(new google.maps.LatLng(37.787, -122.407));
-//datapoints.push(new google.maps.LatLng(37.786, -122.402));
-//datapoints.push(new google.maps.LatLng(37.780, -122.417));
-//datapoints.push(new google.maps.LatLng(37.783, -122.406));
+
 
 addDatapoint(37.787, -122.407);
-addDatapoint(37.786, -122.402);
-addDatapoint(37.780, -122.417);
-addDatapoint(37.783, -122.406);
+addDatapoint(37.798, -122.428);
+addDatapoint(37.776, -122.386);
+addDatapoint(37.776, -122.428);
+addDatapoint(37.798, -122.386);
+
+for(var i = 0; i < 300; i++) {
+    addDatapoint(...genPoint());
+}
 
 
 
---
+++ simulatedPoints/RandomPoints.js
@@ -0,0 +1,22 @@
+var Spread = .8;
+var collectionPoints = 20;
+var random;
+
+var latmin = 37.776; var latmax = 37.798;
+var lngmin = -122.386; var lngmax = -122.428;
+var attractPoints = [];
+
+random = function(minimum, maximum) {
+  return minimum + (maximum - minimum)*Math.random();
+}
+for(var i = 0; i < collectionPoints; i++) {
+  attractPoints.push([random(latmin, latmax), random(lngmin, lngmax)]);
+}
+
+var genPoint = function() {
+  centerpoint = attractPoints[Math.floor(attractPoints.length*Math.random())];
+    angle = random(0, 2*Math.PI);
+    dist = .002*Math.pow(Math.random(),Spread);
+    return [centerpoint[0] + Math.cos(angle)*dist, centerpoint[1] + Math.sin(angle)*dist];
+
+  }
