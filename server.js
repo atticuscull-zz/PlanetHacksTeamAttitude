170d19d4bb9e2f9f3c9f8d359a3fae5def919eef
JudeKratzer <tdujgamer@gmail.com>
2020-05-23 20:07:22 -0700

    yes

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const app = express();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+
+db.defaults({points:[]}).write();
+
+db.get('points')
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + '/Bruh.html')
+    res.sendFile(__dirname + '\bruh.html')
   })
 
 app.listen(8000, () => {
